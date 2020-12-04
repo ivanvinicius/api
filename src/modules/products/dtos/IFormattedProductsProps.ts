93b1ff75469d6c9dcc73c77711d5337789e3bf90
ivanvinicius <ivanvinicius88@gmail.com>
2020-12-04 08:20:39 -0300

    findAllAvoidingDuplicates method ready

---
+++ src/modules/products/dtos/IFormattedProductsProps.ts
@@ -0,0 +1,11 @@
+export default interface IFormattedProductsProps {
+  product_id: string;
+  product_name: string;
+  product_composition: string;
+  brand_id: string;
+  brand_name: string;
+  subcategory_id: string;
+  subcategory_name: string;
+  category_id: string;
+  category_name: string;
+}
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -2,6 +2,7 @@ import { Repository, getRepository } from 'typeorm';
 
 import Product from '@modules/products/infra/typeorm/entities/Product';
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+import IFormattedProductsProps from '@modules/products/dtos/IFormattedProductsProps';
 
 export default class ProductsRepository implements IProductsRepository {
   private ormRepository: Repository<Product>;
@@ -18,45 +19,28 @@ export default class ProductsRepository implements IProductsRepository {
     provider_id: string,
   ): Promise<Product[] | undefined> {
     const query =
-      'SELECT products.id AS product_id, products.name AS product_name, products.composition AS product_composition, brands.id AS brand_id, brands.name AS brand_name, subcategories.id AS subcategory_id, subcategories.name AS subcategory_name, categories.id AS category_id, categories.name AS category_name FROM products JOIN brands ON brands.id = products.brand_id JOIN subcategories ON subcategories.id = products.subcategory_id JOIN categories ON categories.id = subcategories.category_id JOIN products_measures ON products.id != products_measures.product_id WHERE products_measures.provider_id = $1';
+      'SELECT products.id AS product_id, products.name AS product_name, products.composition AS product_composition, brands.id AS brand_id, brands.name AS brand_name, subcategories.id AS subcategory_id, subcategories.name AS subcategory_name, categories.id AS category_id, categories.name AS category_name FROM products JOIN brands ON brands.id = products.brand_id JOIN subcategories ON subcategories.id = products.subcategory_id JOIN categories ON categories.id = subcategories.category_id WHERE products.id NOT IN(SELECT products_measures.product_id FROM products_measures WHERE products_measures.provider_id = $1)';
 
-    return this.ormRepository.query(query, [provider_id]);
+    const products = await this.ormRepository.query(query, [provider_id]);
+
+    const formattedProducts = products.map((item: IFormattedProductsProps) => ({
+      id: item.product_id,
+      name: item.product_name,
+      composition: item.product_composition,
+      brand: {
+        id: item.brand_id,
+        name: item.brand_name,
+      },
+      subcategory: {
+        id: item.subcategory_id,
+        name: item.subcategory_name,
+        category: {
+          id: item.category_id,
+          name: item.category_name,
+        },
+      },
+    }));
+
+    return formattedProducts;
   }
-
-  // public async findAllAvoidingDuplicates(): Promise<Product[] | undefined> {
-  // const products = await this.ormRepository
-  //   .createQueryBuilder()
-  //   .select([
-  //     'products',
-  //     'products_measures',
-  //     'brands',
-  //     'subcategories',
-  //     'categories',
-  //   ])
-  //   .from(Product, 'products')
-  //   .leftJoinAndSelect(
-  //     ProductMeasure,
-  //     'products_measures',
-  //     'products.id != products_measures.product_id',
-  //   )
-  //   .leftJoinAndSelect(
-  //     Brand,
-  //     'brands',
-  //     'brands.id = products.brand_id')
-  //   .leftJoinAndSelect(
-  //     Subcategory,
-  //     'subcategories',
-  //     'subcategories.id = products.subcategory_id',
-  //   )
-  //   .leftJoinAndSelect(
-  //     Category,
-  //     'categories',
-  //     'categories.id = subcategories.category_id',
-  //   )
-  //   .where('products_measures.provider_id = :provider', {
-  //     provider: '591615b0-b4c6-46cb-8296-eb2302865a0b',
-  //   })
-  //   .getMany();
-  // return products;
-  // }
 }
